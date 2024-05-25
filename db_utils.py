190ef05b4621136a01ca62b94ed204b330a29b3c
Mohamed Yislam <msyislam@gmail.com>
2024-05-25 06:25:27 +0400

    intialising class

---
+++ db_utils.py
@@ -0,0 +1,8 @@
+class RDSDatabaseConnector:
+    """
+    This class will contain methods to connect to and extract data from the RDS database.
+    """
+    
+    def __init__(self):
+        # Initialize with any required attributes, such as connection parameters
+        pass
