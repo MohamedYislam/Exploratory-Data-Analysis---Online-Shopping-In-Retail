fc8988e48fa5ba0ed92a02a4ad26da7276000cc8
Mohamed Yislam <msyislam@gmail.com>
2024-06-17 07:40:10 +0400

    formatting files in accordance to PEP 8 standard

--- data_frame_info.py
+++ data_frame_info.py
@@ -1,27 +1,33 @@
 import pandas as pd
 
 class DataFrameInfo:
+    """
+    A class for analyzing a pandas DataFrame.
+    """
+
     def __init__(self, data_frame: pd.DataFrame):
         """
         Initialize the DataFrameInfo class with a DataFrame.
 
         Args:
-            df (pd.DataFrame): The DataFrame to be analyzed.
+            data_frame (pd.DataFrame): The DataFrame to be analyzed.
         """
+        # Initialize the DataFrame
         self.df = data_frame
 
-    def describe_columns(self):
+    def describe_columns(self) -> pd.DataFrame:
         """
         Describe all columns in the DataFrame to check their data types and other summary statistics.
 
         Returns:
             pd.DataFrame: Summary statistics of the DataFrame.
         """
+        # Describe the DataFrame
         description = self.df.describe(include='all')
         print("Column Description:\n", description)
         return description
 
-    def statistics(self, column: str = None):
+    def statistics(self, column: str = None) -> pd.Series:
         """
         Extract median, standard deviation, and mean from the DataFrame or a specified column.
 
@@ -31,11 +37,13 @@ class DataFrameInfo:
         Returns:
             pd.Series or pd.DataFrame: Summary statistics of the column or the entire DataFrame.
         """
-        if not column:  # If no argument was added, return the stats for the entire DataFrame
+        if not column:  
+            # If no argument was added, return the stats for the entire DataFrame
             stats = self.df.describe()
             print("Statistics for the entire DataFrame:\n", stats)
             return stats
-        elif column in self.df.columns:  # If a particular column is specified, return the stats for it alone
+        elif column in self.df.columns:  
+            # If a particular column is specified, return the stats for it alone
             stats = self.df[column].describe()
             print(f"Statistics for column '{column}':\n", stats)
             return stats
@@ -43,7 +51,7 @@ class DataFrameInfo:
             print("The column name you entered does not exist in the DataFrame")
             return None
 
-    def count_distinct_values(self, column: str = None):
+    def count_distinct_values(self, column: str = None) -> int:
         """
         Count distinct values in the DataFrame or a specified categorical column.
 
@@ -53,11 +61,13 @@ class DataFrameInfo:
         Returns:
             int or dict: The count of distinct values in the column or a dictionary of counts for all categorical columns.
         """
-        if not column:  # If no column is specified, return distinct counts for all categorical columns
+        if not column:  
+            # If no column is specified, return distinct counts for all categorical columns
             distinct_counts = {col: self.df[col].nunique() for col in self.df.select_dtypes(include=['category']).columns}
             print("Distinct values for all categorical columns:\n", distinct_counts)
             return distinct_counts
-        elif column in self.df.columns:  # If a particular column is specified, return the distinct count for that column
+        elif column in self.df.columns:  
+            # If a particular column is specified, return the distinct count for that column
             distinct_count = self.df[column].nunique()
             print(f"Distinct values in column '{column}': {distinct_count}")
             return distinct_count
@@ -65,23 +75,25 @@ class DataFrameInfo:
             print("The column name you entered does not exist in the DataFrame")
             return None
 
-    def print_shape(self):
+    def print_shape(self) -> tuple:
         """
         Print the shape of the DataFrame.
 
         Returns:
             tuple: The shape of the DataFrame.
         """
+        # Get the shape of the DataFrame
         shape = self.df.shape
         print(f"Shape of the DataFrame: {shape}")
         return shape
 
-    def null_percentage(self):
+    def null_percentage(self) -> pd.Series:
         """
         Generate a percentage count of NULL values in each column.
 
         Returns:
             pd.Series: A series with the percentage of NULL values for each column.
         """
+        # Calculate the percentage of NULL values for each column
         null_percent = self.df.isnull().mean() * 100
         return null_percent
--- plotter.py
+++ plotter.py
@@ -2,15 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import boxcox, probplot
-from tabulate import tabulate
 import math
+from typing import List
 
 class Plotter:
     """
     A class for generating plots to visualize DataFrame transformations.
     """
 
-    def plot_null_distribution(self, df: pd.DataFrame):
+    def plot_null_distribution(self, df: pd.DataFrame) -> None:
         """
         Plot the distribution of null values in the DataFrame.
 
@@ -29,7 +29,7 @@ class Plotter:
         # Display the plot
         plt.show()
 
-    def plot_null_percentage(self, df: pd.DataFrame):
+    def plot_null_percentage(self, df: pd.DataFrame) -> None:
         """
         Plot the percentage of null values in each column.
 
@@ -58,7 +58,7 @@ class Plotter:
         # Display the plot
         plt.show()
 
-    def plot_column_distribution(self, df: pd.DataFrame, column: str):
+    def plot_column_distribution(self, df: pd.DataFrame, column: str) -> None:
         """
         Plot the distribution of values in a specified column.
 
@@ -80,13 +80,13 @@ class Plotter:
         # Display the plot
         plt.show()
 
-    def plot_skewness(self, df: pd.DataFrame, columns: list):
+    def plot_skewness(self, df: pd.DataFrame, columns: List[str]) -> None:
         """
         Plot the skewness of specified columns in the DataFrame.
 
         Args:
             df (pd.DataFrame): The DataFrame to visualize.
-            columns (list): The list of columns to check skewness.
+            columns (List[str]): The list of columns to check skewness.
         """
         # Number of columns for the grid
         num_columns = len(columns)
@@ -123,13 +123,13 @@ class Plotter:
         # Display the plot
         plt.show()
 
-    def plot_qq(self, df: pd.DataFrame, columns: list):
+    def plot_qq(self, df: pd.DataFrame, columns: List[str]) -> None:
         """
         Plot Q-Q plots of the specified columns in a grid format.
 
         Args:
             df (pd.DataFrame): The DataFrame to visualize.
-            columns (list): The list of columns to plot.
+            columns (List[str]): The list of columns to plot.
         """
         # Number of columns for the grid
         num_columns = len(columns)
@@ -164,22 +164,29 @@ class Plotter:
         # Display the plot
         plt.show()
 
-    def plot_categorical_data(self, df: pd.DataFrame, categorical_columns: list):
+    def plot_categorical_data(self, df: pd.DataFrame, categorical_columns: List[str]) -> None:
         """
         Plot bar charts for categorical columns in a grid format.
 
         Args:
             df (pd.DataFrame): The DataFrame to visualize.
-            categorical_columns (list): List of categorical columns to plot.
+            categorical_columns (List[str]): List of categorical columns to plot.
         """
-        num_columns = 3  # Number of plots per row
+        # Number of plots per row
+        num_columns = 3
         num_rows = math.ceil(len(categorical_columns) / num_columns)
         
+        # Create subplots with a specific size
         fig, axes = plt.subplots(num_rows, num_columns, figsize=(num_columns * 5, num_rows * 5))
+        
+        # Flatten axes array for easy iteration
         axes = axes.flatten()
         
         for i, column in enumerate(categorical_columns):
+            # Use seaborn to create a count plot for each categorical column
             sns.countplot(data=df, x=column, ax=axes[i])
+            
+            # Set the title and labels of the plot
             axes[i].set_title(f'Distribution of {column}')
             axes[i].set_ylabel('Count')
             axes[i].set_xlabel(column)
@@ -188,56 +195,47 @@ class Plotter:
         for j in range(i + 1, len(axes)):
             fig.delaxes(axes[j])
         
+        # Adjust layout
         plt.tight_layout()
+        
+        # Display the plot
         plt.show()
 
-    def plot_boxplots(self, df: pd.DataFrame, columns: list, rows=2, cols=3, figsize=(15, 10)):
+    def plot_boxplots(self, df: pd.DataFrame, columns: List[str], rows: int = 2, cols: int = 3, figsize: tuple = (15, 10)) -> None:
         """
         Plot boxplots for the specified columns.
 
         Args:
             df (pd.DataFrame): The DataFrame to visualize.
-            columns (list): List of columns to plot.
+            columns (List[str]): List of columns to plot.
             rows (int): Number of rows in the grid.
             cols (int): Number of columns in the grid.
             figsize (tuple): Size of the figure.
         """
+        # Create subplots with a specific size
         fig, axes = plt.subplots(rows, cols, figsize=figsize)
+        
+        # Flatten axes array for easy iteration
         axes = axes.flatten()
 
         for i, col in enumerate(columns):
+            # Use seaborn to create a boxplot for each column
             sns.boxplot(y=df[col], ax=axes[i])
+            
+            # Set the title of the plot
             axes[i].set_title(col)
 
         # Hide any unused subplots
         for i in range(len(columns), len(axes)):
             fig.delaxes(axes[i])
 
+        # Adjust layout
         plt.tight_layout()
+        
+        # Display the plot
         plt.show()
 
-    # def plot_qq(self, df: pd.DataFrame, columns: list):
-    #     """
-    #     Plot Q-Q plots of the specified columns in a grid format.
-    #     Args:
-    #         df (pd.DataFrame): The DataFrame to visualize.
-    #         columns (list): The list of columns to plot.
-    #     """
-    #     num_columns = 3  # Number of columns in the grid
-    #     num_rows = (len(columns) + num_columns - 1) // num_columns  # Calculate the number of rows needed
-    #     fig, axes = plt.subplots(num_rows, num_columns, figsize=(15, num_rows * 5))  # Create a grid of subplots
-
-    #     for i, column in enumerate(columns):
-    #         row, col = divmod(i, num_columns)  # Determine the row and column index in the grid
-    #         probplot(df[column].dropna(), dist="norm", plot=axes[row, col])  # Create Q-Q plot
-    #         axes[row, col].get_lines()[1].set_color('red')  # Set the color of the Q-Q plot line to red
-    #         axes[row, col].set_title(f'Q-Q Plot of {column}')  # Set the title of the subplot
-
-    #     plt.tight_layout()  # Adjust subplots to fit into the figure area
-    #     plt.show()  # Display the plot
-
-
-    def correlation_matrix(self, df: pd.DataFrame):
+    def correlation_matrix(self, df: pd.DataFrame) -> None:
         """
         Plot a heatmap of the correlation matrix of the DataFrame.
 
@@ -259,27 +257,34 @@ class Plotter:
         # Display the plot
         plt.show()
 
-
-    def plot_bar_chart(self, df, title, xlabel, ylabel):
+    def plot_bar_chart(self, df: pd.DataFrame, title: str, xlabel: str, ylabel: str) -> None:
         """
-        Plot the number of sales by region in a bar chart.
+        Plot a bar chart.
 
         Args:
             df (pd.DataFrame): The DataFrame containing the data.
+            title (str): Title of the chart.
+            xlabel (str): Label for the x-axis.
+            ylabel (str): Label for the y-axis.
         """
+        # Create a figure with a specific size
         plt.figure(figsize=(12, 6))
+        
+        # Use seaborn to create a bar plot
         sns.barplot(x=df.index, y=df.values, palette="viridis")
 
-        # Add titles and labels
+        # Set the title and labels of the plot
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
+        
+        # Rotate the x-axis labels for better readability
         plt.xticks(rotation=45)
 
         # Display the plot
         plt.show()
 
-    def plot_stacked_bar_chart(self, df, title, xlabel, ylabel):
+    def plot_stacked_bar_chart(self, df: pd.DataFrame, title: str, xlabel: str, ylabel: str) -> None:
         """
         Plot a stacked bar chart.
 
@@ -291,12 +296,16 @@ class Plotter:
         """
         # Reset the index to plot the multi-index DataFrame
         df = df.unstack(level=-1)
+        
+        # Plot the DataFrame as a stacked bar chart
         df.plot(kind='bar', stacked=True, figsize=(12, 6), colormap='viridis')
 
-        # Add titles and labels
+        # Set the title and labels of the plot
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
+        
+        # Rotate the x-axis labels for better readability
         plt.xticks(rotation=45)
         
         # Display the plot
